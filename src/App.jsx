039bdef46949efe865d7d985570510de63a3f726
manishjaiswal1702 <manishjaiswal4043@gmail.com>
2025-06-27 02:39:52 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -150,7 +150,7 @@ function App() {
                 alt=""
               />
               <div className="text text-white flex flex-col gap-3 absolute bottom-60 left-1/2 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
-                <h1 className="text-[12rem] leading-none -ml-40">grand</h1>
+                <h1 className="text-[12rem] leading-none -ml-40">Grand</h1>
                 <h1 className="text-[12rem] leading-none ml-20">theft</h1>
                 <h1 className="text-[12rem] leading-none -ml-40">auto</h1>
               </div>
